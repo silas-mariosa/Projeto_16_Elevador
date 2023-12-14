9d4b4509255efeb684e9273897e28f3736e43f59
Silas Roberto Mariosa de Vasconcelos <113197484+silas-mariosa@users.noreply.github.com>
2023-12-13 23:09:47 -0300

    modified:   index.html 	modified:   script.js 	modified:   style.css

---
+++ script.js
@@ -0,0 +1,11 @@
+const elevator = document.querySelector('.elevator');
+const btnUp = document.getElementById('btnUp');
+const btnDown = document.getElementById('btnDown');
+
+
+btnUp.addEventListener('click', () => {
+
+})
+
+btnDown.addEventListener('click', () => {
+})
