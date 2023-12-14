d3586aff8fc96d10c7ac9c2fd2a82e76ac0f7cf2
silas-mariosa <silas.mariosa@hotmail.com>
2023-12-14 14:41:36 -0300

    update: Reoderned function and logic

--- script.js
+++ script.js
@@ -1,35 +1,69 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const btnUp3 = document.getElementById("btnUp3");
-  const btnDown3 = document.getElementById("btnDown3");
-  const btnUp2 = document.getElementById("btnUp2");
-  const btnDown2 = document.getElementById("btnDown2");
-  const btnUp1 = document.getElementById("btnUp1");
-  const btnDown1 = document.getElementById("btnDown1");
-  const btnUpt = document.getElementById("btnUpt");
+  const callElevator3 = document.getElementById("callElevator3");
+  const callElevator2 = document.getElementById("callElevator2");
+  const callElevator1 = document.getElementById("callElevator1");
+  const callElevatort = document.getElementById("callElevatort");
+  const btnT = document.getElementById("btnT");
+  const btn1 = document.getElementById("btn1");
+  const btn2 = document.getElementById("btn2");
+  const btn3 = document.getElementById("btn3");
 
   const elevator = document.querySelector(".elevator");
-  const currentPosition = () => {
-    elevator.style.top = "679px";
+  let currentPosition = () => {
+    elevator.style.top = "49px";
   };
   currentPosition();
+  const elevtorYPosition = [
+    { ground: 679 },
+    { first: 469 },
+    { second: 259 },
+    { therth: 49 },
+  ];
 
-  const moveElevatorUp = () => {
-    const step = 210;
-    const elevorPosition = parseInt(elevator.style.top);
+  const CallElevator = (floor) => {
+    const selectedLevel = elevtorYPosition.find((level) =>
+      level.hasOwnProperty(floor)
+    );
+    let positionActualy = parseInt(elevator.style.top);
 
-    if (elevorPosition <= 679 && elevorPosition >= 259)
-      elevator.style.top = `${elevorPosition - step}px`;
+    if (positionActualy != selectedLevel[floor]) {
+      let newPositionLevel = `${selectedLevel[floor]}px`;
+
+      console.log("Nova posição: " + newPositionLevel);
+
+      elevator.style.top = `${newPositionLevel}`;
+      console.log(`Elevador movido para o andar '${floor}'`);
+    } else {
+      console.log("O elevador já está no andar!");
+    }
   };
 
-  const moveElevatorDown = () => {
-    const step = 210;
-    const elevorPosition = parseInt(elevator.style.top);
+  const SelectedLevel = (floor) => {
+    let positionActualy = parseInt(elevator.style.top);
+    const selectedLevel = elevtorYPosition.find((level) =>
+      level.hasOwnProperty(floor)
+    );
+    console.log("Position: " + selectedLevel[floor]);
 
-    if (elevorPosition >= 49 &&elevorPosition <= 469)
-      elevator.style.top = `${elevorPosition + step}px`;
+    if (positionActualy != selectedLevel[floor]) {
+      let newPositionLevel = `${selectedLevel[floor]}px`;
+
+      console.log("Nova posição: " + newPositionLevel);
+
+      elevator.style.top = `${newPositionLevel}`;
+      console.log(`Elevador movido para o andar '${floor}'`);
+    } else {
+      console.log("O elevador já está no andar!");
+    }
   };
 
+  btnT.addEventListener("click", () => SelectedLevel("ground"));
+  btn1.addEventListener("click", () => SelectedLevel("first"));
+  btn2.addEventListener("click", () => SelectedLevel("second"));
+  btn3.addEventListener("click", () => SelectedLevel("therth"));
 
-  btnUpt.addEventListener("click", moveElevatorUp);
-  btnDown1.addEventListener("click", moveElevatorDown);
+  callElevatort.addEventListener("click", () => CallElevator("ground"));
+  callElevator1.addEventListener("click", () => CallElevator("first"));
+  callElevator2.addEventListener("click", () => CallElevator("second"));
+  callElevator3.addEventListener("click", () => CallElevator("therth"));
 });
