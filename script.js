dee546825b7d3a5beabe804bbe001c3da5e2922c
Silas Roberto Mariosa de Vasconcelos <113197484+silas-mariosa@users.noreply.github.com>
2023-12-13 23:57:09 -0300

    update

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const elevator = document.querySelector('.elevator');
-const btnUp = document.getElementById('btnUp');
-const btnDown = document.getElementById('btnDown');
+document.addEventListener('DOMContentLoaded', () => {
+    const elevator = document.querySelector('.elevator');
 
+    const moveElevator = (direction) => {
+        const currentTop = parseInt(elevator.style.top) || 0;
+        const step = 210; // Pode ajustar o valor conforme necessário
 
-btnUp.addEventListener('click', () => {
+        if (direction === 'up') {
+            elevator.style.top = `${currentTop - step}px`;
+        } else if (direction === 'down') {
+            elevator.style.top = `${currentTop + step}px`;
+        }
+    };
 
-})
+    // Adiciona ouvintes de eventos para cada botão
+    document.getElementById('btnUp3').addEventListener('click', () => moveElevator('up'));
+    document.getElementById('btnDown3').addEventListener('click', () => moveElevator('down'));
 
-btnDown.addEventListener('click', () => {
-})
+    document.getElementById('btnUp2').addEventListener('click', () => moveElevator('up'));
+    document.getElementById('btnDown2').addEventListener('click', () => moveElevator('down'));
+
+    document.getElementById('btnUp1').addEventListener('click', () => moveElevator('up'));
+    document.getElementById('btnDown1').addEventListener('click', () => moveElevator('down'));
+
+    document.getElementById('btnUpt').addEventListener('click', () => moveElevator('up'));
+});
